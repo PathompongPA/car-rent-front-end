07735314b574e26a7d4b066266957ab6eaf5b915
l.pathompong <pathompong.litk@gmail.com>
2025-09-28 14:40:48 +0700

    edit(resultCar) : adjust result car full edge

--- src/components/frontEnd/resultCar.component.jsx
+++ src/components/frontEnd/resultCar.component.jsx
@@ -19,24 +19,24 @@ export default function ResultCar() {
         )
 
     return (
-        <div className="result-car >> flex flex-col  gap-4 flex-wrap justify-center  | min-w-[150px]  lg:min-h-[300px] w-full lg:gap-4  p-4 | bg-linear-to-b from-white to-blue-2/10  | lg:max-w-7xl md:bg-white md:bg-none snap-start ">
-            <div className="flex flex-wrap justify-center min-h-[50vh] md:min-h-[200px] gap-4 ">
+        <div className="result-car >> flex flex-col  gap-4 flex-wrap justify-center  | min-w-[150px]  lg:min-h-[300px] w-full lg:gap-4  p-4 xl:px-0 md:py-4 | bg-linear-to-b from-white to-blue-2/10  | lg:max-w-7xl md:bg-white md:bg-none snap-start ">
+            <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-center  md:min-h-[200px] gap-4  w-full">
                 {cars?.map(({ carThumbnail, brand, carName, id }, index) =>
                     <Link to={`/car/?id=${id}`} onClick={() => { window.scrollTo(0, 0) }}
                         className="result-car__card --btn >> flex flex-col items-center |
                                 bg-gray-1/40
+                                h-fit
                                 md:bg-transparent
                                   text-blue-2
                                    rounded-lg |
-                                   pb-2
                                   w-full
-                                  sm:w-[300px]
-                                  md:w-[350px]
                                   active:text-white active:bg-blue-2/70 
                                   hover:text-white hover:bg-blue-2/90
+                                  group
+                                  hover:z-20
                                    "
                         key={`${id}+${index}`} >
-                        <img className="result-car__img >> aspect-3/2 lg:aspect-16/9 object-cover | w-full rounded-lg | md:w-full " src={carThumbnail} alt={`thumbnail-${brand.brandName}-${carName}`} />
+                        <img className="result-car__img >> aspect-16/9  object-cover | w-full rounded-lg | md:w-full " src={carThumbnail} alt={`thumbnail-${brand.brandName}-${carName}`} />
                         <h1 className="result-car__title >> w-full text-nowrap text-center overflow-ellipsis overflow-hidden font-thin | md:text-center md:p-4">{`${brand.brandName} ${carName}`}</h1>
                     </Link>
                 )}
