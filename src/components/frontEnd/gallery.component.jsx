56d6ea28aa61f9813088f11eabde7c741a9f6e15
l.pathompong <pathompong.litk@gmail.com>
2025-10-09 16:41:30 +0700

    modify(gallery) : edit size image full size.

--- src/components/frontEnd/gallery.component.jsx
+++ src/components/frontEnd/gallery.component.jsx
@@ -30,25 +30,25 @@ export default function GalleryCar() {
     // console.log("id", Car.data.findIndex((item) => item.id === parameCarId))
 
     return (
-        <div id="gallery" className="gallery-car *** flex flex-col justify-center items-center | pt-17 | md:p-0 xl:gap-4  lg:max-w-7xl xl:min-w-7xl lg:snap-center ">
+        <div id="gallery" className="gallery-car *** flex flex-col justify-center items-center | pt-17 | md:p-0 lg:max-w-7xl xl:min-w-7xl lg:snap-center ">
 
-            <div className="gallery-car__container-title *** grid grid-cols-20 w-full items-center | px-4 | text-title-3 font-black text-blue-1  *:items-center *:flex *:justify-center *:h-12 *:md:h-full ">
-                <button className={`gallery-car__btn-pre --btn col-span-2  ${parameCarId === 0 && "invisible"}`} onClick={() => { setSearchParame({ "id": parameCarId > 0 ? Car.data[parameCarId - 1].id : Car.data[parameCarId].id }) }}>{`<`}</button>
-                <h1 className="gallery-car__title *** select-none text-center col-span-16 text-description-1 h-fit md:text-title-3 xl:text-title-2 text-nowrap truncate">{`${brandName} ${carName}`}</h1>
-                <button className={`gallery-car__btn-next --btn col-span-2 ${amount - 1 === parameCarId && " invisible "} `} onClick={() => { setSearchParame({ "id": parameCarId < amount - 1 ? Car.data[parameCarId + 1].id : Car.data[parameCarId].id }) }}>{`>`}</button>
+            <div className="gallery-car__container-title *** grid grid-cols-20 w-full items-center | px-4 | text-title-3 font-black text-blue-1  *:items-center *:flex *:not-[button]:justify-center *:h-12 *:md:h-full ">
+                <button className={`gallery-car__btn-pre --btn col-span-2 justify-start box-border  p-2  ${parameCarId === 0 && "invisible"}`} onClick={() => { setSearchParame({ "id": parameCarId > 0 ? Car.data[parameCarId - 1].id : Car.data[parameCarId].id }) }}>{`<`}</button>
+                <h1 className="gallery-car__title *** select-none text-center col-span-16 text-description-1 h-fit md:text-description-1 xl:text-title-2 text-nowrap truncate">{`${brandName} ${carName}`}</h1>
+                <button className={`gallery-car__btn-next --btn col-span-2 justify-end box-border p-2 ${amount - 1 === parameCarId && " invisible "} `} onClick={() => { setSearchParame({ "id": parameCarId < amount - 1 ? Car.data[parameCarId + 1].id : Car.data[parameCarId].id }) }}>{`>`}</button>
             </div>
 
-            <div className="gallery-car__container-img *** flex flex-wrap justify-center items-center relative  gap-0 px-4 w-full md:w-[70%] md:gap-4 ">
+            <div className="gallery-car__container-img *** flex flex-wrap justify-center items-center relative  gap-0  px-4  w-full md:w-full md:gap-2  ">
 
-                <h1 className="gallery-car__amount-img ***  hidden select-none h-fit absolute right-[26px] top-[8px] z-10 | md:block md:left-[40px] lg:top-8 lg:bottom-[216px] text-white lg:text-2xl font-black text-shadow-black text-shadow-lg/50">{indexBigImg + 1} / {amountImage} </h1>
+                <h1 className="gallery-car__amount-img ***  hidden select-none h-fit absolute right-8 top-4 z-10 | md:block text-white lg:text-2xl font-black text-shadow-black text-shadow-lg/50">{indexBigImg + 1} / {amountImage} </h1>
                 <img className="gallery-car__big-img *** aspect-16/9 object-cover | md:w-full rounded-lg  hidden md:block " src={Imgs[indexBigImg]} alt="" />
-                <div className="gallery-car__container-mini-img *** flex flex-row justify-start relative overflow-y-hidden overflow-x-scroll snap-x snap-mandatory scroll-smooth | py-0 gap-2 | md:rounded-none md:pt-0 lg:gap-8">
+                <div className="gallery-car__container-mini-img *** flex flex-row justify-start relative overflow-y-hidden overflow-x-scroll snap-x snap-mandatory scroll-smooth | py-0 pb-2 gap-2 | md:rounded-none md:pt-0 lg:gap-2">
                     {Imgs.map((img, index) =>
-                        <img className={`gallery-car__mini-img --btn *** w-[95%] aspect-16/9 md:aspect-16/9 snap-center rounded-lg object-cover | lg:w-[200px] ${indexBigImg === index ? "" : "md:brightness-60 lg:brightness-40"} hover:brightness-95`} src={img} alt={`${brandName}-${carName}-${index}`} key={`${carName}-${index}`} index={index} onClick={handleOnClickMiniImage} />
+                        <img className={`gallery-car__mini-img --btn *** w-[95%] aspect-16/9 md:aspect-16/9 snap-center rounded-lg object-cover | lg:w-[200px] ${indexBigImg === index ? "" : "md:brightness-60 lg:brightness-60"} hover:brightness-100`} src={img} alt={`${brandName}-${carName}-${index}`} key={`${carName}-${index}`} index={index} onClick={handleOnClickMiniImage} />
                     )}
                 </div>
-                <button className="gallery-car__btn-pre-mini-img --btn *** hidden justify-center items-center absolute | p-2 bottom-0 left-0 | md:flex lg:w-[100px] lg:h-[25%] lg:bottom-0 lg:right-0 |  text-shadow-lg/50 text-3xl font-black lg:text-5xl |  text-white select-none | active:text-black/40" onClick={handleClickBtnScrollImage} next="false">{`<`}</button>
-                <button className="gallery-car__btn-next-mini-img --btn *** hidden justify-center items-center absolute | p-2 bottom-0 right-0 | md:flex lg:w-[100px]  lg:h-[25%] | text-3xl font-black lg:text-5xl | text-white select-none | active:text-black/40" onClick={handleClickBtnScrollImage} next="true">{`>`}</button>
+                <button className="gallery-car__btn-pre-mini-img --btn *** hidden justify-center items-center absolute | p-2 bottom-0 left-4 | md:flex lg:w-[100px] lg:h-[18%] lg:bottom-0 lg:right-0 |  text-shadow-lg/50 text-3xl font-black lg:text-4xl |  text-white select-none | active:text-black/40" onClick={handleClickBtnScrollImage} next="false">{`<`}</button>
+                <button className="gallery-car__btn-next-mini-img --btn *** hidden justify-center items-center absolute | p-2 bottom-0 right-4 | md:flex lg:w-[100px]  lg:h-[18%] | text-3xl font-black lg:text-4xl | text-white select-none | active:text-black/40" onClick={handleClickBtnScrollImage} next="true">{`>`}</button>
 
             </div>
 
