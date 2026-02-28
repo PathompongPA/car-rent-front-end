de81a4c0123613b5f50e9ba3256a206f7d5ce508
l.pathompong <pathompong.litk@gmail.com>
2026-02-28 13:33:11 +0700

    refactor : add bg image when no image in gallery

--- src/components/frontEnd/gallery/gallery.style.jsx
+++ src/components/frontEnd/gallery/gallery.style.jsx
@@ -14,7 +14,7 @@ let style = {
     body: cva("flex flex-wrap justify-center items-center relative  gap-0  px-4 xl:px-0  w-full md:w-full md:gap-2  "),
     titleOverlay: cva("  hidden select-none h-fit absolute right-8 top-4 z-10 | md:block text-white lg:text-2xl font-black text-shadow-black text-shadow-lg/50"),
     boxImage: cva([" relative w-full h-full"]),
-    image: cva("aspect-16/9 object-cover | md:w-full rounded-lg  hidden md:block "),
+    image: cva(" bg-black/25  aspect-16/9 object-cover | md:w-full rounded-lg  hidden md:block "),
     images: cva(" flex flex-row justify-start relative overflow-y-hidden overflow-x-scroll snap-x snap-mandatory scroll-smooth | py-0 pb-2 gap-2 | md:rounded-none md:pt-0 lg:gap-2"),
     img: cva([btn, "w-[95%] aspect-16/9 md:aspect-16/9 snap-center rounded-lg object-cover | lg:w-[200px]  hover:brightness-100  brightness-50 data-[is-focus=true]:brightness-100 duration-300 "]),
     btnPreImage: cva([btnbody, "left-4 "]),
